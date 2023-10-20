a2013cb58a75fdda0c6a2a9baf999560ee494e11
Elizabeth Barefoot <elizabeth.barefoot@mrjaffesclass.com>
2023-10-20 11:04:30 -0700

    commit message

--- Fizz-Buzz-copy-copy3/FizzBuzz.java
+++ Fizz-Buzz-copy-copy3/FizzBuzz.java
@@ -12,35 +12,27 @@ public static void main(String[] args)
     int N = 100;
     fizzBuzz(N);
 }
-     static void fizzBuzz(int N)
-{
-   
+     static void fizzBuzz(int N) {
     // Stores count of multiples
     // of 3 and 5 respectively
     int count3 = 0;
     int count5 = 0;
- 
-    // Iterate from 1 to N
     for (int i = 1; i <= N; i++) {
- 
-        // Increment count3 by 1
         count3++;
- 
-        // Increment count5 by 1
         count5++;
- 
-        // Initialize a boolean variable
-        // to check if none of the
-        // condition matches
         boolean flag = false;
- 
+        
+        if (count5 == 5 && count3 == 3) {
+            System.out.println("FizzBuzz");
+            count5 = 0;
+            count3 = 0;
+            flag = true;
+        }
+        
         // Check if the value of count3
         // is equal to 3
         if (count3 == 3) {
-            System.out.print("Fizz");
- 
-            // Reset count3 to 0, and
-            // set flag as True
+            System.out.println("Fizz");
             count3 = 0;
             flag = true;
         }
@@ -48,20 +40,14 @@ public static void main(String[] args)
         // Check if the value of count5
         // is equal  to 5
         if (count5 == 5) {
-            System.out.print("Buzz");
- 
-            // Reset count5 to 0, and
-            // set flag as True
+            System.out.println("Buzz");
             count5 = 0;
             flag = true;
         }
- 
-        // If none of the condition matches
+        
         if (!flag) {
-            System.out.print(i);
+            System.out.println(i);
         }
- 
-        System.out.print(" ");
     }
 }
 }
